8df3cbfa67a35f735089906104395fb3eb652ca2
Mr Bird <4531670+addlistener@users.noreply.github.com>
2024-10-18 09:12:50 +0800

    update abstraction

--- src/shared/Trackers.tsx
+++ src/shared/Trackers.tsx
@@ -4,26 +4,24 @@ import {PRODUCT_DOMAIN} from "../browser/productConstants";
 import Head from "next/head";
 import React from "react";
 
-const getUmamiScript = (src: string, websiteId: string) => {
+const UmamiScript = (props: {src: string, websiteId: string}) => {
   /* avoid gtag removing trackers, recommended by https://umami.is/docs/guides/google-tag-manager */
-  return (
-/* language=javascript */
-`(function () {
+  return (<Head><script>{
+    /* language=javascript */
+    `(function () {
   var el = document.createElement('script');
-  el.setAttribute('src', '${src}');
-  el.setAttribute('data-website-id', '${websiteId}');
+  el.setAttribute('src', '${props.src}');
+  el.setAttribute('data-website-id', '${props.websiteId}');
   document.body.appendChild(el);
 })();`
-  );
+  }</script></Head>);
 };
 
 const Trackers = () => {
   return (<>
       <SpeedInsights/>
       <Analytics/>
-      {PRODUCT_DOMAIN === 'fontsensei.com' && <Head>
-        <script>{getUmamiScript('/um/script.js', 'f2f83fe4-0d44-4f66-8a87-a2d278cc7b7d')}</script>
-      </Head>}
+      {PRODUCT_DOMAIN === 'fontsensei.com' && <UmamiScript src="/um/script.js" websiteId="f2f83fe4-0d44-4f66-8a87-a2d278cc7b7d" />}
     </>
   )
 };
